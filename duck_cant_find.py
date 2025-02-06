6782fc8f290a43415ef6ecdd60c0260ffa3fda51
vbiryukov <vladbiryukov0123@gmail.com>
2025-02-06 21:41:14 +0500

    Готовый рабочий код

--- duck_cant_find.py
+++ duck_cant_find.py
@@ -5,9 +5,8 @@ def test_search_1(open_browser):
     browser.element('[id=react-layout]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 def test_duckduckgo(open_browser):
-    browser.element('[name=q]').clear().type('iejghalkkgларопылдворпыдлвпордлkdfjghskdlfjhsflkdjhдлапорфавдлпрфлдо').press_enter()
+    browser.element('[name=q]').clear().type('kjfhdjkhfjkhssовавлавоаылдаоы18').press_enter()
     browser.element('[id=react-layout]').should(have.text('результаты не найдены.'))
 
     browser.element('[name=q]').clear().type(' ').press_enter()
     browser.element('[id=__next]').should(have.text('No search query text entered. Please try again.'))
-#browser.element('html').should(have.text('About this page'))
